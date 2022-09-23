2fdedddd8213c8150f0d5d7f974a9f24c3a1234b
github-aliceyang <aliyang47@gmail.com>
2022-09-23 11:35:46 -0700

    removed unnecessary files and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Dictionary App
       </header>
+      <footer>This project was coded by Alice Yang and it is <a href="https://github.com/Ayang471/homework-dictionary-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a> and hosted on <a href="/" target="_blank" rel="noreferrer">Netlify</a>.</footer>
     </div>
   );
 }
 
-export default App;
