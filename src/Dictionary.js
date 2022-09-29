05ba8c45e2764de33bb280ef439337e18888de97
github-aliceyang <aliyang47@gmail.com>
2022-09-29 11:32:15 -0700

    added pexels image API and created the photos component and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,33 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 
 export default function Dictionary(props) {
     const [keyword, setKeyword] = useState(props.defaultKeyword);
     const [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
     }
 
-    function search(){
+    function handlePexelsResponse(response) {
+        console.log(response.data);
+        setPhotos(response.data.photos)
+    }
+
+    function search() {
         // documentation: https://dictionaryapi.dev/e
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        const pexelsApiKey = `563492ad6f91700001000001b00ddeedc4a646d6a922f4158be3810a`;
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -35,27 +47,27 @@ export default function Dictionary(props) {
         search();
     }
 
-        if (loaded) {
-            return (
-                <div className="Dictionary">
-                    <section>
-                        <h1>What word do you want to look for?</h1>
-                        <form onSubmit={handleSubmit}>
-                            <input type="search"
-                               defaultValue={props.defaultKeyword}
-                                onChange={handleWordChange}
-                            />
-                        </form>
-                        <div className="hint">
-                            suggested words: yoga, wine, guitar, plant...
-                        </div>
-                    </section>
-                    <Results results={results} />
-                </div>
-            );
-        } else {
-            load();
-            return "Loading..."
-        };
-    }
-    
+    if (loaded) {
+        return (
+            <div className="Dictionary">
+                <section>
+                    <h1>What word do you want to look for?</h1>
+                    <form onSubmit={handleSubmit}>
+                        <input type="search"
+                            defaultValue={props.defaultKeyword}
+                            onChange={handleWordChange}
+                        />
+                    </form>
+                    <div className="hint">
+                        suggested words: yoga, wine, guitar, plant...
+                    </div>
+                </section>
+                <Results results={results} />
+                <Photos photos={photos} />
+            </div>
+        );
+    } else {
+        load();
+        return "Loading..."
+    };
+}
