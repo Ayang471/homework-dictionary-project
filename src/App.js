5c39fe2d12beb2f18ba3ae5003017554e28d1980
github-aliceyang <aliyang47@gmail.com>
2022-09-30 16:23:47 -0700

    made some changed in the styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <Dictionary defaultKeyword="sunrise" />
         </div>
       </header>
-      <footer>This project was coded by <em>Alice Yang</em> and it is open-sourced on <a href="https://github.com/Ayang471/homework-dictionary-project" target="_blank" rel="noreferrer">
+      <footer>This project was coded by Alice Yang and it is open-sourced on <a href="https://github.com/Ayang471/homework-dictionary-project" target="_blank" rel="noreferrer">
         GitHub
       </a> and hosted on <a href="https://creative-tiramisu-c25be9.netlify.app" target="_blank" rel="noreferrer">Netlify</a>.</footer>
     </div>
